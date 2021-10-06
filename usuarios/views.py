d980de548791361b38412aaca35b85da714fc3ab
victor <victor.farias@datasoft.cl>
2021-10-06 13:28:53 -0300

    Agregar usuario con grupo

--- usuarios/views.py
+++ usuarios/views.py
@@ -13,30 +13,56 @@ def home(request):
 
 def RegistroUsuario(request):
     data={
-       'grupos':listar_grupos()
+       'grupos':listar_grupos(),
+       'message':""
     }
     if request.method =='POST':
         #formulario= CustomUserForm(request.POST)
-        user = User.objects.create_user("usuario3","portafolio","usuario3@vfarias.cl")
+        try:
+            
+            username=request.POST.get("username")
+            password=request.POST.get("password")
+            email=request.POST.get("email")
+            id_grupo=int(request.POST.get('groups'))
+            #print(f'{username}, {password} , {email} , {id_grupo}')
+            #v1= listar_grupos()
+            #v1 = [i[0].__str__()+'-'+i[1].__str__() for i in v1]
+            #v2=str.join(';', v1)
+            #data['message']=v2
+            user = User.objects.create_user(username,email,password)
+            group=Group.objects.get(id=id_grupo)
+            user.groups.add(group)
+        except Exception as e:
+            data['message']=e.__str__()
+
         #if formulario.is_valid():
-        print(request.POST.get("username"))
-        print(request.POST.get("groups"))
+        #print(request.POST.get("groups"))
         #formulario.save()
-        return redirect (to='home')
+        #return redirect (to='home')
         
         #else:
         #    print("error")    
     return render(request,'registration/registro.html',data)
 
 def listar_grupos():
-    django_cursor = connection.cursor()
-    cursor = django_cursor.connection.cursor()
-    out_cur = django_cursor.connection.cursor()
-    cursor.callproc("SP_R_GRUPOS",[out_cur])
+
     lista=[]
-    for fila in out_cur:
-        lista.append(fila)
+    #group = Group.objects.values_list()[0][1]
+    size= Group.objects.count()
+    for g in range (size):
+        lista.append(Group.objects.values_list()[g])
     return lista
+    #for g in range (size):
+    #    dic[g]=Group.objects.values_list()[g][1]
+    #return dic
+    #django_cursor = connection.cursor()
+    #cursor = django_cursor.connection.cursor()
+    #out_cur = django_cursor.connection.cursor()
+    #cursor.callproc("SP_R_GRUPOS",[out_cur])
+    #lista=[]
+    #for fila in out_cur:
+    #    lista.append(fila)
+    #return lista
 
 """class RegistroUsuario(CreateView):
     model = User
