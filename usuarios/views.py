54f83875d04abe3f2a007c33cc3e51cb36983f4c
victor <victor.farias@datasoft.cl>
2021-10-06 17:52:13 -0300

    Comentarios y limpieza de codigo del views de usuario

--- usuarios/views.py
+++ usuarios/views.py
@@ -6,33 +6,42 @@ from django.db import connection
 from django.contrib.auth import login, logout
 from django.contrib.auth import authenticate
 
-# Create your views here.
 
+#Vista que retorna Home
 def home(request):
     return render(request,'usuarios/home.html')
 
+#Vista del Home
 def log(request):
-
-    
+    #Si se hace una solicitud de tipo POST    
     if request.method =='POST':
-
-        us=request.POST.get("username")
-        pas=request.POST.get("password1")
-        user = authenticate(username=us, password=pas)
-        login(request,user)
+        #Obtiene los datos mandados por el formulario en POST
+        usr=request.POST.get("username")
+        pwd=request.POST.get("password1")
+        #Autentica el usuario y nos devuelve el usuario
+        user = authenticate(username=usr, password=pwd)
         if user is None:
             print("no hay usuario")
         else:
+            #Iniciamos sesion con el usuario
+            login(request,user)
+            #Obtenemos su grupo y segun esto, le redireccionamos a otra pagina
             if user.groups.filter(name='Administrador').exists():
                 return redirect (to='mesas')
             else:
                 return redirect (to='home')
     return render(request,'registration/login.html') 
+
+#Vista del logout
+#Lo que hace simplemente es terminar la sesion y redirigir al home
 def logout_def(request):
     logout(request)
     return redirect (to='home')
 
+#Vista del Registro del Usuario
 def RegistroUsuario(request):
+    #Este es un diccionario que se le pasará por parametro y se le incrustan
+    #datos que metamos, en este caso, incrustamos una lista de tuplas y un mensaje
     data={
        'grupos':listar_grupos(),
        'message':""
@@ -40,57 +49,31 @@ def RegistroUsuario(request):
     if request.method =='POST':
         #formulario= CustomUserForm(request.POST)
         try:
-            
+            #Obtenemos usuario,contraseña, email y el  id del grupo
             username=request.POST.get("username")
             password=request.POST.get("password1")
-            print(password)
             email=request.POST.get("email")
             id_grupo=int(request.POST.get('groups'))
-            #print(f'{username}, {password} , {email} , {id_grupo}')
-            #v1= listar_grupos()
-            #v1 = [i[0].__str__()+'-'+i[1].__str__() for i in v1]
-            #v2=str.join(';', v1)
-            #data['message']=v2
+            #Creamos el usuario
             user = User.objects.create_user(username,email,password)
+            #Traemos el grupo que seleccionaron
             group=Group.objects.get(id=id_grupo)
+            #Agregamos al usuario a un grupo
             user.groups.add(group)
-            login(request,user)
+            #Lo redirigimos al login
+            return redirect(to='login')
             
         except Exception as e:
-            data['message']=e.__str__()
-
-        #if formulario.is_valid():
-        #print(request.POST.get("groups"))
-        #formulario.save()
-        #return redirect (to='home')
-        
-        #else:
-        #    print("error")    
+            data['message']=e.__str__()   
     return render(request,'registration/registro.html',data)
-
+#Metodo que devuelve la lista de los grupos
 def listar_grupos():
-
+    #Creamos una lista vacia que devolveremos
     lista=[]
-    #group = Group.objects.values_list()[0][1]
+    #Obtenemos la cantidad total de grupos
     size= Group.objects.count()
+    #Iteramos segun la cantidad de grupos y añadimos uno por uno los grupos
     for g in range (size):
         lista.append(Group.objects.values_list()[g])
+    #Finalmente retornamos la lista
     return lista
-    #for g in range (size):
-    #    dic[g]=Group.objects.values_list()[g][1]
-    #return dic
-    #django_cursor = connection.cursor()
-    #cursor = django_cursor.connection.cursor()
-    #out_cur = django_cursor.connection.cursor()
-    #cursor.callproc("SP_R_GRUPOS",[out_cur])
-    #lista=[]
-    #for fila in out_cur:
-    #    lista.append(fila)
-    #return lista
-
-"""class RegistroUsuario(CreateView):
-    model = User
-    template_name= "registration/registro.html"
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('mesas')
-"""
