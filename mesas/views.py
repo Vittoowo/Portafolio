9603ce70fd6bbf712cf3885dd4baad76311c649c
Vittoowo <victor.farias.bravo@gmail.com>
2021-10-07 15:53:44 -0300

    añadido contenido de urls.py y views.py Administrador a Mesas

--- mesas/views.py
+++ mesas/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.db import connection
 # Create your views here.
+from django.contrib.auth.decorators import login_required
+@login_required
+def Mesas(request):
+    data ={
+        'estado':listar_estados()
+    }
+    return render(request,'Mesas.html',data)
+
+def listar_estados():
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor()
+    out_cur = django_cursor.connection.cursor()
+    cursor.callproc("SP_R_ESTADO_MESA",[out_cur])
+    lista=[]
+    for fila in out_cur:
+        lista.append(fila)
+    return lista
