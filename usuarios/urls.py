3c8bd3b459368e1ca75e4064cf720ed4a715f7d6
Vittoowo <victor.farias.bravo@gmail.com>
2021-10-07 18:31:56 -0300

    modificaciones views usuarios

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from .views import home, RegistroUsuario,log,logout_def
-from Administrador import views
-from mesas import views
-from reservas import views
+from Administrador import views as av
+from mesas import views as mv
+from reservas import views as rv
 
 urlpatterns = [
     path('', home,name='home'),
     path('registro/',RegistroUsuario,name="registro"),
-    path('Administrador/',views.Mesas,name="mesas"),
+    path('Administrador/',av.Mesas,name="mesas"),
     path('login/',log,name='login'),
     path('logout/',logout_def,name='logout'),
 
