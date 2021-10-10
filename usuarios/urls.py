a6e15ea5d96361e9dd67e63e5dda7c8491d48799
Vittoowo <victor.farias.bravo@gmail.com>
2021-10-10 18:40:02 -0300

    cambio en urls mesas

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -7,7 +7,7 @@ from reservas import views as rv
 urlpatterns = [
     path('', home,name='home'),
     path('registro/',RegistroUsuario,name="registro"),
-    path('Administrador/',av.Mesas,name="mesas"),
+    path('Administrador/Mesas',mv.Mesas,name="mesas"),
     path('login/',log,name='login'),
     path('logout/',logout_def,name='logout'),
     #Agregar Reservas
