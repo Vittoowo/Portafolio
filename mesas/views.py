2a9fe55efde3a700bc0443fe35ab3bf08aed4024
Vittoowo <victor.farias.bravo@gmail.com>
2021-10-10 17:57:17 -0300

    Metodo Agregar Mesas + proc al en BDD

--- mesas/views.py
+++ mesas/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.db import connection
 # Create your views here.
 from django.contrib.auth.decorators import login_required
+
+
 @login_required
 def Mesas(request):
     data ={
@@ -10,14 +12,39 @@ def Mesas(request):
     return render(request,'Mesas.html',data)
 
 def listar_estados():
+
     django_cursor = connection.cursor()
+
     cursor = django_cursor.connection.cursor()
+
     out_cur = django_cursor.connection.cursor()
+
     cursor.callproc("SP_R_ESTADO_MESA",[out_cur])
+    
     lista=[]
+
     for fila in out_cur:
         lista.append(fila)
     return lista
+
+"""
+def CreateMesas(request):
+
+    django_cursor = connection.cursor()
+
+    cursor = django_cursor.connection.cursor()
+
+    out_cur = 0;
+
+    cursor.callproc("SP_CREATE_MESAS",[request. ,2,1,    out_cur])
+
+    if out_cur == 1:
+        return render(request, 'mesas/Mesas.html')
+    else:
+        return render(request,'Mesas.html')
+
+
+"""
 
 
 #lo del video
