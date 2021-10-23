6dd93f73d457c70e363073de1311b4e7496fa6ec
Cris <cancino.cristian@hotmail.com>
2021-10-23 02:57:26 -0300

    Views a Models (Reservas) y Correcion Eliminar

--- reservas/models.py
+++ reservas/models.py
@@ -1,3 +1,66 @@
+from django.contrib.auth.models import User, Group
 from django.db import models
+from django.db import connection
+from django.shortcuts import render
+import cx_Oracle
 
-# Create your models here.
+class Reservas():
+    def __init__(self,ID_RESERVA, ESTADO_RESERVA, RUT_RESERVA, FECHA_RESERVA, EMAIL_RESERVA, TELEFONO_RESERVA, CANTIDAD_PERSONAS_RESERVAS):
+        self.ID_RESERVA = ID_RESERVA
+        self.ESTADO_RESERVA = ESTADO_RESERVA
+        self.RUT_RESERVA = RUT_RESERVA
+        self.FECHA_RESERVA = FECHA_RESERVA
+        self.EMAIL_RESERVA = EMAIL_RESERVA
+        self.TELEFONO_RESERVA = TELEFONO_RESERVA
+        self.CANTIDAD_PERSONAS_RESERVAS = CANTIDAD_PERSONAS_RESERVAS
+
+        #Listado de estado de reserva
+    def listar_estados():
+        django_cursor = connection.cursor()
+        cursor = django_cursor.connection.cursor()
+        out_cur = django_cursor.connection.cursor()
+        cursor.callproc("SP_R_ESTADO_RESERVA",[out_cur])
+        lista=[]
+        for fila in out_cur:
+            lista.append(fila)
+        return lista
+
+
+        #Listado de Reservas
+    def listado_reservas():
+        django_cursor = connection.cursor()
+        cursor = django_cursor.connection.cursor()
+        out_cur = django_cursor.connection.cursor()
+
+        cursor.callproc("SP_LISTAR_RESERVAS", [out_cur])
+
+        lista = []
+        for fila in out_cur:
+            lista.append(fila)
+
+        return lista
+
+        #Guardar Reserva
+    def agregar_reserva(ID_RESERVA, estado_reserva_id_est_reserva, rut_reserva, fecha_reserva, email, telefono_reserva, cantidad_personas_reserva):
+        django_cursor = connection.cursor()
+        cursor = django_cursor.connection.cursor()
+        salida = cursor.var(cx_Oracle.NUMBER)
+        cursor.callproc('SP_AGREGAR_RESERVA',[ID_RESERVA,estado_reserva_id_est_reserva, rut_reserva, fecha_reserva, email, telefono_reserva, cantidad_personas_reserva, salida])
+        return salida.getvalue()
+
+
+        #ELiminar Reserva
+    def eliminar_reserva(ID_RESERVA):
+        django_cursor = connection.cursor()
+        cursor = django_cursor.connection.cursor()
+        salida = cursor.var(cx_Oracle.NUMBER)
+        cursor.callproc('SP_ELIMINAR_RESERVA',[ID_RESERVA, salida])
+        return salida.getvalue()
+
+        #Modificar Reserva
+    def modificar_reserva(ID_RESERVA, estado_reserva_id_est_reserva, rut_reserva, fecha_reserva, email, telefono_reserva, cantidad_personas_reserva):
+        django_cursor = connection.cursor()
+        cursor = django_cursor.connection.cursor()
+        salida = cursor.var(cx_Oracle.NUMBER)
+        cursor.callproc('SP_MODIFICAR_RESERVA',[ID_RESERVA,estado_reserva_id_est_reserva, rut_reserva, fecha_reserva, email, telefono_reserva, cantidad_personas_reserva, salida])
+        return salida.getvalue()
--- reservas/views.py
+++ reservas/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import redirect, render
 from django.db import connection
+from django.contrib.auth.decorators import login_required
 import cx_Oracle
+from reservas.models import Reservas as reservas
 # Create your views here.
 
-
+@login_required
 def Reservas(request):
     data ={
-        'Lista_Estado_Reserva':listar_estados(),
-        'Reserva':listado_reservas()
+        'Lista_Estado_Reserva':reservas.listar_estados(),
+        'Reserva':reservas.listado_reservas()
     }
     
     if 'Guardar' in request.POST:
@@ -18,20 +20,20 @@ def Reservas(request):
         EMAIL = request.POST.get('Email')
         TELEFONO_RESERVA = request.POST.get('Telefono')
         CANTIDAD_PERSONAS_RESERVA = request.POST.get('CantidadPersonas')
-        salida = agregar_reserva(ID_RESERVA, ESTADO_RESERVA_ID_EST_RESERVA, RUT_RESERVA, FECHA_RESERVA, EMAIL, TELEFONO_RESERVA, CANTIDAD_PERSONAS_RESERVA)
+        salida = reservas.agregar_reserva(ID_RESERVA, ESTADO_RESERVA_ID_EST_RESERVA, RUT_RESERVA, FECHA_RESERVA, EMAIL, TELEFONO_RESERVA, CANTIDAD_PERSONAS_RESERVA)
         if salida == 1:
             data['Mensaje'] = 'Reserva Agregada'
-            data['Reserva'] = listado_reservas()
+            data['Reserva'] = reservas.listado_reservas()
         else:
             data['Mensaje'] = 'No se ha podido guardar'
         return render(request, 'Reservas.html', data)
     
     elif 'Eliminar' in request.POST:
         ID_RESERVA = request.POST.get('IDReserva')
-        salida = eliminar_reserva (ID_RESERVA)
+        salida = reservas.eliminar_reserva (ID_RESERVA)
         if salida == 1:
             data['Mensaje'] = 'Reserva Eliminada'
-            data['Reserva'] = listado_reservas()
+            data['Reserva'] = reservas.listado_reservas()
         else:
             data['Mensaje'] = 'No se ha podido eliminar'
         return render(request, 'Reservas.html', data)
@@ -44,10 +46,10 @@ def Reservas(request):
         EMAIL = request.POST.get('Email')
         TELEFONO_RESERVA = request.POST.get('Telefono')
         CANTIDAD_PERSONAS_RESERVA = request.POST.get('CantidadPersonas')
-        salida = modificar_reserva(ID_RESERVA, ESTADO_RESERVA_ID_EST_RESERVA, RUT_RESERVA, FECHA_RESERVA, EMAIL, TELEFONO_RESERVA, CANTIDAD_PERSONAS_RESERVA)
+        salida = reservas.modificar_reserva(ID_RESERVA, ESTADO_RESERVA_ID_EST_RESERVA, RUT_RESERVA, FECHA_RESERVA, EMAIL, TELEFONO_RESERVA, CANTIDAD_PERSONAS_RESERVA)
         if salida == 1:
             data['Mensaje'] = 'Reserva Modificada'
-            data['Reserva'] = listado_reservas()
+            data['Reserva'] = reservas.listado_reservas()
         else:
             data['Mensaje'] = 'No se ha podido modificar'
         return render(request, 'Reservas.html', data)
@@ -55,49 +57,3 @@ def Reservas(request):
     
 
 
-def listar_estados():
-    django_cursor = connection.cursor()
-    cursor = django_cursor.connection.cursor()
-    out_cur = django_cursor.connection.cursor()
-    cursor.callproc("SP_R_ESTADO_RESERVA",[out_cur])
-    lista=[]
-    for fila in out_cur:
-        lista.append(fila)
-    return lista
-
-def listado_reservas():
-    django_cursor = connection.cursor()
-    cursor = django_cursor.connection.cursor()
-    out_cur = django_cursor.connection.cursor()
-
-    cursor.callproc("SP_LISTAR_RESERVAS", [out_cur])
-
-    lista = []
-    for fila in out_cur:
-        lista.append(fila)
-
-    return lista
-
-
-def agregar_reserva(ID_RESERVA, estado_reserva_id_est_reserva, rut_reserva, fecha_reserva, email, telefono_reserva, cantidad_personas_reserva):
-    django_cursor = connection.cursor()
-    cursor = django_cursor.connection.cursor()
-    salida = cursor.var(cx_Oracle.NUMBER)
-    cursor.callproc('SP_AGREGAR_RESERVA',[ID_RESERVA,estado_reserva_id_est_reserva, rut_reserva, fecha_reserva, email, telefono_reserva, cantidad_personas_reserva, salida])
-    return salida.getvalue()
-
-
-
-def eliminar_reserva(ID_RESERVA):
-    django_cursor = connection.cursor()
-    cursor = django_cursor.connection.cursor()
-    salida = cursor.var(cx_Oracle.NUMBER)
-    cursor.callproc('SP_ELIMINAR_RESERVA',[ID_RESERVA, salida])
-    return salida.getvalue()
-
-def modificar_reserva(ID_RESERVA, estado_reserva_id_est_reserva, rut_reserva, fecha_reserva, email, telefono_reserva, cantidad_personas_reserva):
-    django_cursor = connection.cursor()
-    cursor = django_cursor.connection.cursor()
-    salida = cursor.var(cx_Oracle.NUMBER)
-    cursor.callproc('SP_MODIFICAR_RESERVA',[ID_RESERVA,estado_reserva_id_est_reserva, rut_reserva, fecha_reserva, email, telefono_reserva, cantidad_personas_reserva, salida])
-    return salida.getvalue()
