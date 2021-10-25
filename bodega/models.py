ee0460a11940ffa82964ef35e1e56e5b58ace66f
Yensyuwu <yens.gonzalez@duocuc.cl>
2021-10-25 16:25:40 -0300

    Metodo agregar productos correspondientes a la bodega

--- bodega/models.py
+++ bodega/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.contrib.auth.models import User, Group
+from django.db import connection
+from django.shortcuts import render
+import cx_Oracle
 # Create your models here.
+
+
+#bodega v_id_prod in NUMBER,
+    
+class Bodega():
+    def __init__(self,nombre_prod,marca_prod,proveedor_prod,precio_compra,ultima_compra):
+        self.nombre_prod = nombre_prod
+        self.marca_prod=marca_prod
+        self.proveedor_prod=proveedor_prod
+        self.precio_compra= precio_compra
+        self.ultima_compra= ultima_compra
+
+    def agregar_producto(self):
+            try:
+                django_cursor = connection.cursor()
+                cursor = django_cursor.connection.cursor()
+                salida= cursor.var(cx_Oracle.NUMBER)
+                cursor.callproc ('SP_CREATE_PRODUCTOS',[self.nombre_prod,self.marca_prod,self.proveedor_prod,
+                self.precio_compra,self.ultima_compra,salida])
+                return salida.getvalue()
+            except Exception as e:
+                raise e.__str__()
--- bodega/views.py
+++ bodega/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from django.db import connection
+from django.contrib.auth.decorators import login_required
+from bodega.models import Bodega as bodega
 # Create your views here.
+
+@login_required
+def Bodega(request):
+    data ={
+        'mensaje': "",
+    }
+
+    if 'Guardar' in request.POST:
+            id_prod = int(request.POST.get('id_prod'))
+            marca_prod= request.post.get('marca_prod')
+            nombre_prod=request.POST.get('nombre_prod')
+            proveedor_prod=request.POST.get('estado')
+            precio_compra= int(request.POST.get('precio_compra'))
+            ultima_compra = int(request.POST.get('ultima_compra'))
+            prod_c=bodega(id_prod,marca_prod,proveedor_prod,precio_compra,ultima_compra) #creando el objeto
+            salida = prod_c.agregar_producto()#llamamos al metodo del models, creamos una variable y le asigna el valor que va a retornar
+            if salida==1:
+                print 
+                data['mensaje'] = 'Producto agregado correctamente👍'
+            else:
+                data['mensaje'] = f'ERROR, no se pudo agregar el producto correctamente😢'
+            return render(request, 'Bodega.html', data)
