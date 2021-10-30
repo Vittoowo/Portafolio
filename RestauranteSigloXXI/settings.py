30bb411811fe204e33dc3554ca4e6ebeb0f64bc4
Yensyuwu <yens.gonzalez@duocuc.cl>
2021-10-30 12:45:21 -0300

    Corrección de error database

--- RestauranteSigloXXI/settings.py
+++ RestauranteSigloXXI/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'RestauranteSigloXXI.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
@@ -90,7 +90,7 @@ DATABASES = {
         'PORT': '1521',
     }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
