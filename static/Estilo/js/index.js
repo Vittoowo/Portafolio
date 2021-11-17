1754895b2348d849575835805a66081969d1b841
Matiaspv <matiasrpv1@gmail.com>
2021-11-16 21:56:52 -0300

    Se agregan imagenes y carousel al home (css y js)

--- static/Estilo/js/index.js
+++ static/Estilo/js/index.js
@@ -7,3 +7,20 @@ navToggle.addEventListener("click",() => {
 });
 
 // Final del js del navar//
+
+// JS Del carousel //
+window.addEventListener('load',function()
+{
+    new Glider(document.querySelector('.carousel-lista'),{
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        
+        dots: '.carousel-indicadores',
+        arrows: {
+        prev: '.carousel-anterior',
+        next: '.carousel-siguiente'
+        }
+    });
+});
+
+ // Fin del js del carousel //
