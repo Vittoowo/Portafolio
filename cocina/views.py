5cfe952dea873394c4391c5f9022b01922b3c3e1
Victor <victor.farias.bravo@gmail.com>
2021-11-28 17:23:27 -0300

    generado el template de agregar plato

--- cocina/views.py
+++ cocina/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from bodega.models import Producto 
 
 # Create your views here.
 def gestionPlatos(request):
-    return render(request,'gestion-platos.html')
+    data = {"Producto":Producto.listar_productos()}
+    return render(request,'gestion-platos.html',data)
