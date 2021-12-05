a032d3a7d3929a3a91fb9de19191bbb256466c44
Victor <victor.farias.bravo@gmail.com>
2021-12-05 18:25:15 -0300

    Unidades de medida agregadas a gestion de platos

--- cocina/views.py
+++ cocina/views.py
@@ -3,5 +3,5 @@ from bodega.models import Producto
 
 # Create your views here.
 def gestionPlatos(request):
-    data = {"Producto":Producto.listar_productos()}
+    data = {"Producto":Producto.listar_productos(), "UnidadMedidaProducto":Producto.listar_unidades_medida()}
     return render(request,'gestion-platos.html',data)
